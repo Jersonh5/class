caea62d654545a9cf02bebcb92508a00b41f04a3
Jersonh5 <161898583+Jersonh5@users.noreply.github.com>
2024-03-05 21:32:02 -0500

    Update Partida.java

--- src/main/java/com/example/demo/prueba/Partida.java
+++ src/main/java/com/example/demo/prueba/Partida.java
@@ -12,7 +12,17 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.JoinTable;
 import jakarta.persistence.ManyToMany;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter
+@Getter
+@Table
 @Entity
 public class Partida {
     @Id 
