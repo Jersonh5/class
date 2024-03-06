750776ed2e6b0f103ebdbb1a485a2a568e5060aa
Jersonh5 <161898583+Jersonh5@users.noreply.github.com>
2024-03-05 21:36:36 -0500

    Delete src/main/java/com/example/demo/prueba/repositorios directory

--- src/main/java/com/example/demo/prueba/repositorios/UsuarioRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.prueba.repositorios;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.demo.prueba.Usuario;
-
-public interface UsuarioRepository extends JpaRepository<Usuario, Long>{
-    
-}
