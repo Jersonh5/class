e75e7f12b9942e4d0339b5a386c61f28d2b6d4a2
Jersonh5 <161898583+Jersonh5@users.noreply.github.com>
2024-03-05 21:31:18 -0500

    Update Mensaje.java

--- src/main/java/com/example/demo/prueba/Mensaje.java
+++ src/main/java/com/example/demo/prueba/Mensaje.java
@@ -8,7 +8,17 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter
+@Getter
+@Table
 @Entity
 public class Mensaje {
     @Id 
